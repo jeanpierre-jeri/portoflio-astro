2a9360fcbf27b180cc9db090559cf8e91077e990
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-03 16:45:05 -0500

    feat: extract api call to services

---
+++ src/services/joke.ts
@@ -0,0 +1,15 @@
+export const getJoke = async () => {
+  try {
+
+    const response = await fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json",
+      }
+    })
+    const { joke } = await response.json();
+
+    return joke
+  } catch (error) {
+    return 'Circles are pointless.'
+  }
+}
